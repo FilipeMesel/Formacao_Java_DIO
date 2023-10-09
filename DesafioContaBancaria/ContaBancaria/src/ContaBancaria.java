b6f2236a8330cac8a68b616c5c7b6eda7fd43a63
FilipeMesel <lipemesel@hotmail.com>
2023-10-08 21:29:41 -0300

    add readme

--- DesafioContaBancaria/ContaBancaria/src/ContaBancaria.java
+++ DesafioContaBancaria/ContaBancaria/src/ContaBancaria.java
@@ -5,8 +5,12 @@ public class ContaBancaria {
     public static void main(String [] args)
     {
         Scanner s = new Scanner(System.in);
+
+        System.out.println("Digite sua agência: ");
         String agencia = s.next();
+        System.out.println("Digite sua conta: ");
         int conta = s.nextInt();
+        System.out.println("Digite seu saldo: ");
         double saldo = s.nextDouble();
 
         System.out.println("Obrigado por criar uma conta aqui! Sua agencia " +
